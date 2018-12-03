32ef3f5e471a84584c993678e72e526cfa7d0190
masahitojp <randomstep@gmail.com>
2018-12-04 01:35:22 +0900

    bump update 0.0.2

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name="fabric3-stubs",
-    version="0.0.1",
-    packages=["fabric3-stubs"],
+    version="0.0.2",
+    packages=["fabric-stubs"],
     package_data={
         "fabric-stubs": ["*.pyi"],
     },
